ed3c13bcbabd7dc1e3de14290aabb2abb2bca229
AustralCoder <jnicolaskelly.2@gmail.com>
2026-07-09 19:34:43 -0300

    feat: add logic to lotation filtering system

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const jobsListingsSection = document.querySelector('.jobs-listings');
 
-jobsListingsSection.addEventListener('click', (event) =>{
+jobsListingsSection?.addEventListener('click', (event) =>{
     const element = event.target
 
     if (element.classList.contains('button-apply-job')){
@@ -9,3 +9,34 @@ jobsListingsSection.addEventListener('click', (event) =>{
         element.disabled = true
     }
 });
+
+const detailCardsSection = document.querySelector('.details-card');
+
+
+detailCardsSection?.addEventListener('click', (event) => {
+    const element = event.target
+
+    if (element.classList.contains('button-apply-job')){
+        element.textContent = 'aplicado!'
+        element.classList.add('is-applied')
+        element.disabled = true
+    }
+});
+
+
+const locationFilter = document.querySelector('#location');
+const cards = document.querySelectorAll('.job-card');
+
+locationFilter.addEventListener('change', (event) =>{
+    const chosenLocation = event.target.value
+
+    cards.forEach(card => {
+
+        const cardLocation = card.dataset.location;
+        if (chosenLocation === 'all' || chosenLocation.toLowerCase() === cardLocation.toLowerCase()) {
+            card.style.display = 'flex';
+        } else {
+            card.style.display = 'none';
+        }
+    })
+})
