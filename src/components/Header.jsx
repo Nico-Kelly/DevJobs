6548469b067a1788cc193f60e4967d7a163ef2b0
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-08-29 22:16:48 -0300

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+                <div className="logo">
+                    <h1>DevJobs</h1>
+                </div>
+
+
+                <ul>
+                    <li>
+                        <a href="#jobs">Trabajos</a>
+                    </li>
+                    <li>
+                        <a href="#about">Acerca</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
+
